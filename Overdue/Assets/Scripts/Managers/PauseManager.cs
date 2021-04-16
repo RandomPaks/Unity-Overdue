ffb658d7170e297dff0aee79a2d9fa6d2736185e
Paolo Roxas <paoloroxas1@gmail.com>
2021-04-16 13:01:32 +0800

    pause manager text functionality

--- Overdue/Assets/Scripts/Managers/PauseManager.cs
+++ Overdue/Assets/Scripts/Managers/PauseManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
     bool isPaused = false;
     [SerializeField] GameObject pauseUI;
+    [SerializeField] Button resumeButton;
+    [SerializeField] Button quitButton;
 
     public static PauseManager Instance { get; private set; }
 
@@ -22,11 +25,13 @@ public class PauseManager : MonoBehaviour
             if (this.isPaused)
             {
                 this.pauseUI.SetActive(true);
+                GameManager.Instance.toggleCursorLock(false);
                 GameManager.Instance.SetState(GameState.PAUSED);
             }
             else
             {
                 this.pauseUI.SetActive(false);
+                GameManager.Instance.toggleCursorLock(true);
                 GameManager.Instance.SetState(GameState.GAME);
             }
         }
@@ -37,6 +42,12 @@ public class PauseManager : MonoBehaviour
     {
         this.pauseUI.SetActive(false);
         this.isPaused = false;
+        GameManager.Instance.toggleCursorLock(true);
         GameManager.Instance.SetState(GameState.GAME);
     }
+
+    public void OnEnterResumeButton() => resumeButton.GetComponentInChildren<Text>().fontSize = 60;
+    public void OnExitResumeButton() => resumeButton.GetComponentInChildren<Text>().fontSize = 48;
+    public void OnEnterQuitButton() => quitButton.GetComponentInChildren<Text>().fontSize = 60;
+    public void OnExitQuitButton() => quitButton.GetComponentInChildren<Text>().fontSize = 48;
 }

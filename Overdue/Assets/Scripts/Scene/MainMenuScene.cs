5c25ab8a7441709994719624fc3f738b74c6051f
Paolo Roxas <paoloroxas1@gmail.com>
2021-04-26 16:56:05 +0800

    fixed main menu

---
+++ Overdue/Assets/Scripts/Scene/MainMenuScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class MainMenuScene : MonoBehaviour
+{
+    //Only needs one text to get the font shader
+    [SerializeField] TMP_Text newGame, loadGame, exit; 
+
+    void Start()
+    {
+        newGame.fontMaterial.SetFloat("_FaceDilate", -1.0f);
+        loadGame.fontMaterial.SetFloat("_FaceDilate", -1.0f);
+        exit.fontMaterial.SetFloat("_FaceDilate", -1.0f);
+
+        newGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
+        loadGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
+        exit.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
+    }
+}

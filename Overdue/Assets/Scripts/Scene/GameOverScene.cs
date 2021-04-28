0cb08172f13c998cca9a4401fb645d78e70f6378
Paolo Roxas <paoloroxas1@gmail.com>
2021-04-28 08:56:37 +0800

    added a main menu bgm and game over voice

--- Overdue/Assets/Scripts/Scene/GameOverScene.cs
+++ Overdue/Assets/Scripts/Scene/GameOverScene.cs
@@ -7,7 +7,6 @@ public class GameOverScene : MonoBehaviour
 {
     [SerializeField] TMP_Text gameOver, continueGame, mainMenu;
     [SerializeField] float secs = 2.5f;
-    bool fade = false;
 
     void Start()
     {
@@ -15,11 +14,11 @@ public class GameOverScene : MonoBehaviour
         continueGame.fontMaterial.SetFloat("_FaceDilate", -1.0f);
         mainMenu.fontMaterial.SetFloat("_FaceDilate", -1.0f);
 
-        continueGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
-        mainMenu.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
+        continueGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs);
+        mainMenu.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs);
         FadeInGameOver();
     }
-    void FadeInGameOver() => gameOver.fontMaterial.DOFloat(0.0f, "_FaceDilate", 2.5f).OnComplete(() => FadeOutGameOver());
+    void FadeInGameOver() => gameOver.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs).OnComplete(() => FadeOutGameOver());
 
-    void FadeOutGameOver() => gameOver.fontMaterial.DOFloat(-0.25f, "_FaceDilate", 2.5f).OnComplete(() => FadeInGameOver());
+    void FadeOutGameOver() => gameOver.fontMaterial.DOFloat(-0.25f, "_FaceDilate", secs).OnComplete(() => FadeInGameOver());
 }
--- Overdue/Assets/Scripts/Scene/MainMenuScene.cs
+++ Overdue/Assets/Scripts/Scene/MainMenuScene.cs
@@ -4,7 +4,8 @@ using DG.Tweening;
 
 public class MainMenuScene : MonoBehaviour
 {
-    [SerializeField] TMP_Text newGame, loadGame, exit; 
+    [SerializeField] TMP_Text newGame, loadGame, exit;
+    [SerializeField] float secs = 2.5f;
 
     void Start()
     {
@@ -12,8 +13,8 @@ public class MainMenuScene : MonoBehaviour
         loadGame.fontMaterial.SetFloat("_FaceDilate", -1.0f);
         exit.fontMaterial.SetFloat("_FaceDilate", -1.0f);
 
-        newGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
-        loadGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
-        exit.fontMaterial.DOFloat(0.0f, "_FaceDilate", 3.0f);
+        newGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs);
+        loadGame.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs);
+        exit.fontMaterial.DOFloat(0.0f, "_FaceDilate", secs);
     }
 }

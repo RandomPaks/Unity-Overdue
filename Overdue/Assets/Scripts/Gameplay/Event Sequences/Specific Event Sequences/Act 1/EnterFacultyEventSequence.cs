f37a458d5213760ce1dc678f4bfbc09c3ea90565
clawy <raina_cally_ang@dlsu.edu.ph>
2021-05-24 15:51:43 +0800

    finished act 1 events

---
+++ Overdue/Assets/Scripts/Gameplay/Event Sequences/Specific Event Sequences/Act 1/EnterFacultyEventSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterFacultyEventSequence : AEventSequence
+{
+    [SerializeField] GameObject noteObject;
+    [SerializeField] GameObject keyObject;
+    [SerializeField] GameObject sandraCharacter;
+
+    public override void PlayEvent()
+    {
+        this.noteObject.SetActive(true);
+        this.keyObject.SetActive(true);
+        this.sandraCharacter.SetActive(false);
+        this.OnFinishEvent();
+    }
+
+    public override void OnFinishEvent()
+    {
+        base.OnFinishEvent();
+        this.gameObject.SetActive(false);
+    }
+}

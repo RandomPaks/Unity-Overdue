6bb70d8f7f67f3bf9ace64d2fba3923245651adb
Paolo Roxas <paoloroxas1@gmail.com>
2021-06-02 23:53:49 +0800

    fixed keys

---
+++ Overdue/Assets/Scripts/Scene/FlickeringLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light light;
+    [SerializeField] float minTime = 1f;
+    [SerializeField] float maxTime = 5f;
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    // Update is called once per frame
+    IEnumerator Flicker()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+            light.enabled = !light.enabled;
+        }
+    }
+}
